a37282d1cabe264e554c91f58cfbda11dfb3bec7
Kamaluddin <97792683+SayedKamaluddin@users.noreply.github.com>
2025-02-25 14:15:27 -0600

    put the clock code in github and vscode

---
+++ Color-Time-Swap/sketch.js
@@ -0,0 +1,34 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let waitTime = 2000;
+let lastSwitchedTime = 0;
+let isWhite = true;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  swapStateIfNeded()
+  showBackground()
+}
+
+function swapStateIfNeded(){
+  if (millis()>lastSwitchedTime+waitTime){
+    isWhite = !isWhite;
+    lastSwitchedTime += waitTime;
+  }
+}
+
+function showBackground(){
+  if (isWhite){
+    background("white")
+  }else{
+    background("black")
+  }
+}
