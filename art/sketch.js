f0e32be0725c203ab4423b9804355d4cb0fa2037
Kamaluddin <97792683+SayedKamaluddin@users.noreply.github.com>
2025-03-07 11:28:27 -0600

    Art Demo

--- art/sketch.js
+++ art/sketch.js
@@ -2,17 +2,26 @@
 //Using Object Notation and Arrays
 // March 07, 2025
 
+const LINESIZE = 50;
 let lineArray = [];
 let someLine;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  someLine = spownLine(width/2, height/2, 400);
+  for (let x= 0; x< width; x+=LINESIZE){
+    for (let y= 0; y< height; y+=LINESIZE){
+      someLine = spownLine(x, y, LINESIZE);
+      lineArray.push(someLine);
+    }
+  }
+  // noLoop();
 }
 
 function draw() {
   background(220);
-  line(someLine.x1,someLine.y1,someLine.x2,someLine.y2);
+  for (let aLine of lineArray){
+    line(aLine.x1,aLine.y1,aLine.x2,aLine.y2);
+  }
 }
 
 function spownLine(x, y, theSize){
