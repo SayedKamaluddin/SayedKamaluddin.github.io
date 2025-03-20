254ce12e40ef91af6746d1cff11282d4471badbe
Kamaluddin <97792683+SayedKamaluddin@users.noreply.github.com>
2025-03-20 14:17:00 -0600

    added comments sound effects and made the texts better

--- ao-assignment/sketch.js
+++ ao-assignment/sketch.js
@@ -1,10 +1,11 @@
-// Project Title
-// Your Name
-// Date
+// Cross the River
+// kamaluddin Hashimy
+// March 20, 2025
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Moving, Wining, and Losing each has diffrent sound effects
 
+// Defining all the varibles and seting the values
 let avgSpeed = 1;
 let laneSize = 100;
 let level = 1;
@@ -20,13 +21,23 @@ let onBlock = true;
 let dis;
 let levelDone = false;
 let levelFailed = false;
+let jumpingSound, losingSound, winingSound; 
 
+//preloads all the images from the directory
+function preload(){
+  jumpingSound = loadSound("../jump.mp3");
+  losingSound = loadSound("../losing.mp3");
+  winingSound = loadSound("../wining.mp3");
+}
+
+// setup function
 function setup() {
   createCanvas(windowWidth, windowHeight);
   stroke(255,255,255,80);
   setupBlocksNLanes();
 }
 
+// draw function
 function draw() {
   background('green');
   drawingLanes();
@@ -39,11 +50,14 @@ function draw() {
   }
 }
 
+// draws the movement text and level
 function startingText(){
-  myText(35, 'Just Cross the River', width/2-150,50);
-  myText(25, 'Use W,S,D,A to move',width/2-130,80);
+  myText(35, 'Just Cross the River', width/2,50);
+  myText(25, 'Use W,S,D,A to move',width/2,80);
+  myText(25, `Level: ${level}`, 100, 50);
 }
 
+// draws the lanes from the lanes array
 function drawingLanes(){
   fill(56, 175, 205);
   for (let lane of lanes){
@@ -51,6 +65,7 @@ function drawingLanes(){
   }
 }
 
+// draws all the blocks on the lanes from the blocks array and make them move
 function drawingBlocks(){
   fill(164, 84, 48);
   for (let block of blocks){
@@ -65,6 +80,7 @@ function drawingBlocks(){
   }
 }
 
+// when called creates an object with the correct values to draw a lane
 function spownLane(rectX){
   let theLane = {
     x: rectX,
@@ -75,30 +91,37 @@ function spownLane(rectX){
   return theLane;
 }
 
+// draws the charachter
 function character(){
   fill('red');
   circle( charX, charY, charSize);
 }
 
+// checks the keys that are pressed and moves the character accordingly 
 function keyPressed(){
   if (keyIsDown(87) && charY > 80){
     charY-=laneSize/2;
     onBlock = true;
+    jumpingSound.play();
   }
   else if (keyIsDown(83) && charY < height-100){
     charY+=laneSize/2;
     onBlock = true;
+    jumpingSound.play();
   }
   else if (keyIsDown(68)){
     charX+=laneSize;
     onBlock = true;
+    jumpingSound.play();
   }
   else if (keyIsDown(65) && charX > 100){
     charX-=laneSize;
     onBlock = true;
+    jumpingSound.play();
   }
 }
 
+// makes the character move with the blocks when they're on it and prevents them from falling off also checks if they lost or not
 function stayOnBlock(){
   for(let block of blocks){
     if(charX>block.x && charX<block.x+block.w && charY>block.y && charY<block.y+block.h){
@@ -114,6 +137,7 @@ function stayOnBlock(){
   }
 }
 
+// when called creates an object with the correct values to draw a block
 function spownBlocks(blockX, blockY, directions){
   let theBlock = {
     x: blockX+10,
@@ -125,6 +149,7 @@ function spownBlocks(blockX, blockY, directions){
   return theBlock;
 }
 
+// stops the game when we run out of space in the screen
 function testIfGameIsFinished(){
   let finishLevel = width-100/laneSize;
   if (level===finishLevel){
@@ -132,14 +157,18 @@ function testIfGameIsFinished(){
   }
 }
 
+// checks the characters location and if has crossed the road stops the game and raises level
 function won(){
   level++;
+  avgSpeed += 0.5;
   numOfLane = 3+level-1;
-  myText(35, "Nice, DoubleClick to Start the new Level", width/2-25, height/2);
+  myText(35, "Nice, DoubleClick to Start the new Level", width/2, height/2);
   levelDone = true;
+  winingSound.play();
   noLoop();
 }
 
+// sets up everything when is called and draws the lanes and blocks again after each lever
 function setupBlocksNLanes(){
   blocks = [];
   lanes = [];
@@ -162,6 +191,7 @@ function setupBlocksNLanes(){
   }
 }
 
+// sets up everything necessary for the new level and resumes the game when called
 function startNewLevel(){
   if (levelDone){
     setupBlocksNLanes();
@@ -169,13 +199,15 @@ function startNewLevel(){
   }
 }
 
+// stops the game when is called 
 function lost(){
   noLoop();
-  level = 0;
-  myText(35, "You Lose, DoubleClick to try again", width/2-300, height/2);
+  losingSound.play();
+  myText(35, "You Lose, DoubleClick to try again", width/2, height/2);
   levelFailed = true;
 }
 
+// moves the character to the original postion and resuems the game when called
 function restart(){
   if (levelFailed){
     levelFailed = false;
@@ -185,15 +217,20 @@ function restart(){
   }
 }
 
+// calls the the functions after double clicking
 function doubleClicked(){
   startNewLevel();
   restart();
 }
 
+// an easier way to write texts in the screen
 function myText(size, sentence, x, y){
   textSize(size);
-  fill(0);
-  text(sentence, x-3, y-3);
-  fill("yellow");
-  text(sentence, x, y);
+  w = textWidth(sentence)/2;
+  fill(113,12,4);
+  text(sentence, x-w-3, y-3);
+  fill(59,231,236);
+  text(sentence, x-w-1, y);
+  fill(255);
+  text(sentence, x-w, y);
 }
