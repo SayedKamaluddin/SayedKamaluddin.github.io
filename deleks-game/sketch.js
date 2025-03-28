584c01df1f6df85cd26a661441d5b186f0e6e405
Kamaluddin <97792683+SayedKamaluddin@users.noreply.github.com>
2025-03-28 14:16:44 -0600

    Created the work space for the project and still working on it

--- deleks-game/sketch.js
+++ deleks-game/sketch.js
@@ -1,95 +1,157 @@
-// 2D Array Grid Demo
+// 2D Array Rectangle Grid Demo
+// Pick a cell size, then fill the screen with as many as possible.
+// This will likely be rectangular instead of square...
 
-// let grid = [[0,1,1,0],
-//             [1,1,0,0],
-//             [0,0,1,1],
-//             [0,1,0,0]];
-// const CELL_SIZE = 100;   //do this if you are just choosing a size
-let cellSize = 25;
-const SQUARE_DIMENSIONS = 10;
+const CELL_SIZE = 25;
+let level=1;
 let grid;
+let rows;
+let cols;
+const OPEN_TILE = 0;
+const ALIVE_ENEMY = 1;
+const DEAD_ENEMY = 2;
+const PLAYER = 9;
+let enemies = [];
+let thePlayer = {
+  x:0,
+  y:0,
+};
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(700, 700);
+  cols = Math.ceil(width/CELL_SIZE);
+  rows = Math.ceil(height/CELL_SIZE);
+  grid = generateGrid(cols, rows);
 
-  //make the largest square that fits
-  // if (height > width) {
-  //   cellSize = width / SQUARE_DIMENSIONS;
-  // }
-  // else {
-  //   cellSize = height / SQUARE_DIMENSIONS;
-  // }
-
-  grid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
+  thePlayer.x = rows/2;
+  thePlayer.y = cols/2;
+  grid[thePlayer.y][thePlayer.x] = PLAYER;
 }
 
 function draw() {
   background(220);
-
   displayGrid();
 }
 
-function mousePressed(){
-  let x = Math.floor(mouseX/cellSize);
-  let y = Math.floor(mouseY/cellSize);
-  
-  toggleCell(x,y);
+function genarateEnemies(){
+  for(let i; i< level+3; i++){
+    let theEnemy = {
+      x:random(rows),
+      y:random(cols),
+    };
+    grid[y][x]=1;
+    enemies.push(theEnemy);
+  }
+}
+
+function keyPressed(){
+  if(key === '8'){
+    movePlayer(thePlayer.x, thePlayer.y-1);
+  }
+  else if(key === '2'){
+    movePlayer(thePlayer.x, thePlayer.y+1);
+  }
+  else if(key === '4'){
+    movePlayer(thePlayer.x-1, thePlayer.y);
+  }
+  else if(key === '6'){
+    movePlayer(thePlayer.x+1, thePlayer.y);
+  }
+  else if(key === '7'){
+    movePlayer(thePlayer.x-1, thePlayer.y-1);
+  }
+  else if(key === '9'){
+    movePlayer(thePlayer.x+1, thePlayer.y-1);
+  }
+  else if(key === '1'){
+    movePlayer(thePlayer.x-1, thePlayer.y+1);
+  }
+  else if(key === '3'){
+    movePlayer(thePlayer.x+1, thePlayer.y+1);
+  }
+  else if(key === '5'){
+    movePlayer(thePlayer.x, thePlayer.y);
+  }
+}
+
+function movePlayer(x,y){
+  if(x >= 0 && x< cols && y >= 0 && y < rows && grid [y][x] === OPEN_TILE){
+    
+    //previos player location 
+    let oldX = thePlayer.x;
+    let oldY = thePlayer.y;
+    
+    //reset the old spot to be open
+    grid[oldY][oldX] = OPEN_TILE;
+    
+    // keep track of where the player is 
+    thePlayer.x = x;
+    thePlayer.y = y;
+    
+    
+    grid[thePlayer.y][thePlayer.x] = PLAYER;
+  }
 }
 
 function toggleCell(x,y){
-  if (grid[y][x]===0){
-    grid[y][x]=1;
-  }
-  else if (grid[y][x]===1){
-    grid[y][x]=0;
-  }
-}
+  // make sure cell you're toggling is actually in the grid
+  if (x>=0 && x < cols && y>=0 && y < rows){
+    if (grid[y][x]===OPEN_TILE){
+      grid[y][x]=DEAD_ENEMY;
+    }
+    else if (grid[y][x]===DEAD_ENEMY){
+      grid[y][x]=OPEN_TILE;
+    }
 
-function keyPressed() {
-  if (key === "r") {
-    grid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
-  }
-  if (key === "e") {
-    grid = generateGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
   }
 }
 
 function displayGrid() {
-  for (let y = 0; y < SQUARE_DIMENSIONS; y++) {
-    for (let x = 0; x < SQUARE_DIMENSIONS; x++) {
-      if (grid[y][x] === 1) {
-        fill("black");
-      }
-      else if (grid[y][x] === 0) {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === OPEN_TILE) {
         fill("white");
       }
-      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      else if (grid[y][x] === DEAD_ENEMY) {
+        fill("black");
+      }
+      else if (grid[y][x] === PLAYER) {
+        fill("red");
+      }
+      
+      
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
     }
   }
 }
+
+// function displayGrid() {
+//   for (let y = 0; y < rows; y++) {
+//     for (let x = 0; x < cols; x++) {
+//       if (grid[y][x] === OPEN_TILE) {
+//         fill("white");
+//       }
+//       else if (grid[y][x] === DEAD_ENEMY) {
+//         fill("black");
+//       }
+//       else if (grid[y][x] === PLAYER) {
+//         fill("red");
+//       }
+      
+      
+//       square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+//     }
+//   }
+// }
 
 function generateGrid(cols, rows) {
   let newGrid = [];
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
+      //toss a 0 or 1 in randomly
       newGrid[y].push(0);
-    }
-  }
-  return newGrid;
-}
-
-function generateRandomGrid(cols, rows) {
-  let newGrid = [];
-  for (let y = 0; y < rows; y++) {
-    newGrid.push([]);
-    for (let x = 0; x < cols; x++) {
-      if (random(100) < 50) {
-        newGrid[y].push(0);
-      }
-      else {
-        newGrid[y].push(1);
-      }
     }
   }
   return newGrid;
